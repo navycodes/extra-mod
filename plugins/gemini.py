f7d71fbe086ff5569570e933e82cd73bf54fce40
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-02 06:38:32 +0700

    update plugins/gemini.py

--- plugins/gemini.py
+++ plugins/gemini.py
@@ -129,7 +129,7 @@ async def _(c: nlx, m, _):
     if not text:
         return pros.edit(_("enc_5").format(em.gagal))
     url = "https://next-nolimit-api-app.vercel.app/api/flux-image-gen-beta/"
-    payload = {"prompt": text}
+    payload = {"model": "flux", "prompt": text}
     response = requests.post(url, json=payload)
     if response.status_code == 200:
         with open("genai.jpg", "wb") as f:
