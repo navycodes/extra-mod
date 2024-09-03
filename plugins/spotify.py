d421e616f88e250bcfa89310d144eeacd572331a
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-03 20:02:58 +0700

    update plugins/spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -42,6 +42,7 @@ async def _(c: nlx, m, _):
     em = Emojik(c)
     em.initialize()
     pros = await m.reply(_("proses").format(em.proses))
+    
     if m.command[1] == "-dl":
         query = m.command[2]
         if len(m.command) < 3 or not query.startswith("https"):
@@ -51,11 +52,12 @@ async def _(c: nlx, m, _):
             return await pros.delete()
         await download_spot(c, m, query)
         return await pros.delete()
+    
     else:
-        cmd = m.text.split()[1]
+        cmd = " ".join(m.text.split()[1:])
         xk = {"_id": c.me.id, "args": cmd}
         udB.set_var(c.me.id, "spot", xk)
         print(cmd)
         x = await c.get_inline_bot_results(bot_username, f"src_spot {cmd}")
         await m.reply_inline_bot_result(x.query_id, x.results[0].id)
-        # return await pros.delete()
+        return await pros.delete()
