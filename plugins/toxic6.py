6efbaf80b972de4264191d9fad6be2e86e8da2f0
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-04 11:10:16 +0700

    update plugins/toxic6.py

--- plugins/toxic6.py
+++ plugins/toxic6.py
@@ -14,27 +14,7 @@ from pyrogram import *
 from Userbot import *
 
 
-@ky.ubot("mix")
-async def _(c: nlx, m, _):
-    uputt = await m.reply("**Hai... Perkenalkan Saya Adalah Mix-Userbot**")
-    asyncio.sleep(3)
-    await uputt.edit("**Userbot base on Pyrogram**")
-    asyncio.sleep(2)
-    await uputt.edit("**Part Of @KynanSupport... Salam Kenal yaaa ><**")
-    asyncio.sleep(3)
-    await uputt.edit(
-        "**Repository [Mix-Userbot](https://github.com/naya1503/Mix-Userbot)**"
-    )
-
-
 # Create by myself @AyiinXd
-
-
-@ky.ubot("sayang")
-async def _(c: nlx, m, _):
-    xx = await m.reply("**Aku Cuma Mau Bilang...**", reply_to_message_id=ReplyCheck(m))
-    asyncio.sleep(3)
-    await xx.edit("**Aku Sayang Kamu Mwaahh** 😘❤")
 
 
 # Create by myself @AyiinXd
@@ -48,7 +28,7 @@ async def _(c: nlx, m, _):
     await asyncio.sleep(1.8)
     await uputt.edit("**Tetaplah Bernafas...**")
     await asyncio.sleep(1.8)
-    await uputt.edit("**Dan Bersyukur...**")
+    return await uputt.edit("**Dan Bersyukur...**")
 
 
 # Create by myself @AyiinXd
@@ -62,7 +42,7 @@ async def _(c: nlx, m, _):
     await asyncio.sleep(1.8)
     await uputt.edit("**Tetaplah Mengeluh...**")
     await asyncio.sleep(1.8)
-    await uputt.edit("**Dan Putus Asa...**")
+    return await uputt.edit("**Dan Putus Asa...**")
 
 
 # Create by myself @AyiinXd
