1a5db7f319163729b39c2b85a53dd7d7336b0f6d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-04 11:30:21 +0700

    update plugins/sticker.py

--- plugins/sticker.py
+++ plugins/sticker.py
@@ -22,32 +22,29 @@ async def _(c: nlx, m, _):
     rep = m.reply_to_message
     stick = rep.sticker
     if not rep:
-        await m.reply(_("st_1").format(em.gagal))
-        return
+        return await m.reply(_("st_1").format(em.gagal))
+        
     else:
         if stick.is_video == True:
             pat = await c.download_media(stick, file_name=f"{stick.set_name}.mp4")
-            await rep.reply_document(
+            return await rep.reply_document(
                 document=pat,
                 caption=_("st_2").format(
                     em.sukses, stick.emoji, em.alive, stick.file_id
                 ),
             )
         elif stick.is_animated == True:
-            await m.reply(_("st_1").format(em.gagal))
-            return
+            return await m.reply(_("st_1").format(em.gagal))
+            
 
         else:
             pat = await c.download_media(stick, file_name=f"{stick.set_name}.png")
-            await rep.reply_document(
+            return await rep.reply_document(
                 document=pat,
                 caption=_("st_2").format(
                     em.sukses, stick.emoji, em.alive, stick.file_id
                 ),
             )
-        return
-    os.remove(pat)
-    return
 
 
 @ky.ubot("unkang")
@@ -57,19 +54,18 @@ async def _(self: nlx, m, _):
     rep = m.reply_to_message
     await self.unblock_user(bot_username)
     if not rep:
-        await m.reply(_("st_3").format(em.gagal))
-        return
+        return await m.reply(_("st_3").format(em.gagal))
+        
     if rep.sticker:
         pros = await m.reply(_("proses").format(em.proses))
         ai = await self.forward_messages(bot_username, m.chat.id, message_ids=rep.id)
         await self.send_message(bot_username, "/unkang", reply_to_message_id=ai.id)
         await asyncio.sleep(0.5)
         if await resleting(self, m) == "Stiker berhasil dihapus dari paket Anda.":
-            await pros.edit(_("st_4").format(em.sukses))
-            return
+            return await pros.edit(_("st_4").format(em.sukses))
         else:
-            await pros.edit(_("st_5").format(em.gagal))
-            return
+            return await pros.edit(_("st_5").format(em.gagal))
+            
     else:
         return await m.reply(_("st_6").format(em.gagal))
 
@@ -82,8 +78,8 @@ async def _(self: nlx, m, _):
     cekemo = self.get_arg(m)
     await self.unblock_user(bot_username)
     if not rep:
-        await m.reply(_("st_7").format(em.gagal))
-        return
+        return await m.reply(_("st_7").format(em.gagal))
+        
     await self.send_message(bot_username, "/kang")
     pros = await m.reply(_("proses").format(em.proses))
     if len(m.command) == 2:
@@ -106,11 +102,10 @@ async def _(self: nlx, m, _):
         ):
             await asyncio.sleep(5)
             await tai.copy(m.chat.id)
-        await ai.delete()
-        await pros.delete()
-        ulat = await self.resolve_peer(bot_username)
-        await self.invoke(DeleteHistory(peer=ulat, max_id=0, revoke=False))
-        return
+    await ai.delete()
+    await pros.delete()
+    ulat = await self.resolve_peer(bot_username)
+    return await self.invoke(DeleteHistory(peer=ulat, max_id=0, revoke=False))
 
 
 async def resleting(self, m):
