9ec4885b51e694a0e98282f1fb572fcaf82f3500
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-04 12:05:11 +0700

    update plugins/sangmata.py

--- plugins/sangmata.py
+++ plugins/sangmata.py
@@ -37,6 +37,8 @@ async def _(c: nlx, m, _):
             argu = int(m.command[1])
         except Exception as err:
             return await m.reply(_("err").format(em.gagal, err))
+    except Exception as err:
+        return await m.reply(_("err").format(em.gagal, err))
     proses = await m.reply(_("proses").format(em.proses))
     sg = "@SangMata_beta_bot"
     try:
@@ -50,7 +52,7 @@ async def _(c: nlx, m, _):
         if respon.text == None:
             continue
         if not respon:
-            await m.reply(_("sangmat_3").format(em.gagal))
+            return await m.reply(_("sangmat_3").format(em.gagal))
         elif respon:
             await m.reply(_("sangmat_4").format(em.sukses, respon.text))
             break
@@ -61,5 +63,4 @@ async def _(c: nlx, m, _):
     except Exception:
         pass
 
-    await proses.delete()
-    return
+    return await proses.delete()
