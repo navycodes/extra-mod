2fe4f88b480e659857857505538df9da6a61e7a2
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-04 13:43:27 +0700

    update plugins/games.py

--- plugins/games.py
+++ plugins/games.py
@@ -8,7 +8,7 @@ def help_string(org):
 
 
 @ky.ubot("catur")
-async def _(c, m):
+async def _(c, m, _):
     try:
         x = await c.get_inline_bot_results("GameFactoryBot")
         msg = m.reply_to_message or m
@@ -22,36 +22,36 @@ async def _(c, m):
 
 
 @ky.ubot("dice")
-async def _(c, m):
+async def _(c, m, _):
     await c.send_dice(m.chat.id, "🎲")
     return await m.delete()
 
 
 @ky.ubot("dart")
-async def _(c, m):
+async def _(c, m, _):
     await c.send_dice(m.chat.id, "🎯")
     return await m.delete()
 
 
 @ky.ubot("basket")
-async def _(c, m):
+async def _(c, m, _):
     await c.send_dice(m.chat.id, "🏀")
     return await m.delete()
 
 
 @ky.ubot("bowling")
-async def _(c, m):
+async def _(c, m, _):
     await c.send_dice(m.chat.id, "🎳")
     return await m.delete()
 
 
 @ky.ubot("football")
-async def _(c, m):
+async def _(c, m, _):
     await c.send_dice(m.chat.id, "⚽")
     return await m.delete()
 
 
 @ky.ubot("slot")
-async def _(c, m):
+async def _(c, m, _):
     await c.send_dice(m.chat.id, "🎰")
     return await m.delete()
