39940edd43b5d297112e52e59852a75cbb19c764
naya1503 <naya1503@users.noreply.github.com>
2024-09-05 06:38:37 +0000

    Auto Fix

--- plugins/chats.py
+++ plugins/chats.py
@@ -1,4 +1,3 @@
-
 import asyncio
 
 from Userbot import *
@@ -8,8 +7,7 @@ __MODULES__ = "Chats"
 
 def help_string(org):
     return h_s(org, "help_cc")
-    
-    
+
 
 @ky.ubot("cekmember")
 async def _(c, m, _):
@@ -23,8 +21,8 @@ async def _(c, m, _):
         return await pros.edit("{} Total members group {}".format(em.sukses, o))
     except Exception as e:
         return await pros.edit(_("err_1").format(em.gagal, str(e)))
-        
-        
+
+
 @ky.ubot("cekonline")
 async def _(c, m, _):
     em = Emojik(c)
@@ -37,8 +35,8 @@ async def _(c, m, _):
         return await pros.edit("{} Total members online group {}".format(em.sukses, o))
     except Exception as e:
         return await pros.edit(_("err_1").format(em.gagal, str(e)))
-        
-        
+
+
 @ky.ubot("cekmsg")
 async def _(c, m, _):
     em = Emojik(c)
@@ -55,6 +53,10 @@ async def _(c, m, _):
     try:
         o = await c.search_messages_count(chat_id, from_user=user_id)
         await asyncio.sleep(1)
-        return await pros.edit("{} Total message from user {} is {} messages".format(em.sukses, umention, o))
+        return await pros.edit(
+            "{} Total message from user {} is {} messages".format(
+                em.sukses, umention, o
+            )
+        )
     except Exception as e:
         return await pros.edit(_("err_1").format(em.gagal, str(e)))
