e75f9951de682c4ef3780324e627df7093109998
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-09 12:14:58 +0700

    update plugins/graph.py

--- plugins/graph.py
+++ plugins/graph.py
@@ -135,7 +135,7 @@ async def _(client: nlx, message, _):
 
 
 async def upload_media(m):
-    media = m.reply_to_message.download()
+    media = await m.reply_to_message.download()
     url = "https://itzpire.com/tools/upload"
     # headers = {"accept": "*/*", "Content-Type": "multipart/form-data"}
     with open(media, "rb") as file:
