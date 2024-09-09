d904607c43ba40bbb7052e9714d4a85e4c432994
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-09 12:28:40 +0700

    update plugins/graph.py

--- plugins/graph.py
+++ plugins/graph.py
@@ -12,6 +12,7 @@ from io import BytesIO
 
 from PIL import Image
 from Userbot import Emojik, fetch, h_s, ky, nlx
+from telegraph.aio import Telegraph
 
 __MODULES__ = "Telegraph"
 
@@ -19,119 +20,22 @@ __MODULES__ = "Telegraph"
 def help_string(org):
     return h_s(org, "help_graph")
 
-
-async def dl_thumbnail(client, message):
-    if message.reply_to_message and message.reply_to_message.sticker:
-        sticker = message.reply_to_message.sticker
-        if sticker.thumbs:
-            thumb_id = sticker.thumbs[0].file_id
-            thumb_path = await client.download_media(thumb_id)
-
-            # Konversi thumbnail ke format JPEG
-            with Image.open(thumb_path) as img:
-                img = img.convert("RGB")
-                thumb_io = BytesIO()
-                img.save(thumb_io, format="JPEG")
-                thumb_io.seek(0)
-
-            os.remove(thumb_path)
-            return thumb_io, "image/jpeg"
-
-    return await dl_pic(client, message.reply_to_message)
-
-
-async def dl_pic(client, media):
-    path = await client.download_media(media)
-    mime_type = None
-
-    if media.photo:
-        mime_type = "image/jpeg"
-    elif media.video:
-        mime_type = "video/mp4"
-    elif media.animation or media.sticker:
-        mime_type = "image/gif"  # Anggap sebagai GIF jika animasi
-    elif media.document:
-        mime_type = "application/octet-stream"
-
-    if not mime_type:
-        return None, None
-
-    with open(path, "rb") as f:
-        content = f.read()
-
-    os.remove(path)
-
-    get_media = BytesIO(content)
-    get_media.name = os.path.basename(path)
-
-    return get_media, mime_type
-
-
-"""
-@ky.ubot("tg")
-async def _(client: nlx, message, _):
-    emo = Emojik(client)
-    emo.initialize()
-    XD = await message.reply(_("proses").format(emo.proses))
-    if not message.reply_to_message:
-        return await XD.edit(_("grp_1").format(emo.gagal))
-    telegraph = TelegraphAPI()
-    if message.reply_to_message.media:
-        try:
-            media_type = message.reply_to_message.media
-            media_data, mime_type = None, None
-
-            if media_type == MessageMediaType.STICKER:
-                media_data, mime_type = await dl_thumbnail(client, message)
-            elif media_type in [
-                MessageMediaType.PHOTO,
-                MessageMediaType.VIDEO,
-                MessageMediaType.ANIMATION,
-                MessageMediaType.DOCUMENT,
-            ]:
-                media_data, mime_type = await dl_pic(client, message.reply_to_message)
-
-            if media_data is None:
-                await message.delete()
-                return await XD.delete()
-
-            media_data.seek(0)
-            response = requests.post(
-                "https://telegra.ph/upload",
-                files={"file": ("file", media_data, mime_type)},
-            ).json()
-
-            if "error" in response:
-                return await XD.edit(f"<code>{response['error']}</code>")
-
-            media_url = response[0]["src"]
-            page_content = [
-                {"tag": "img", "attrs": {"src": f"https://telegra.ph{media_url}"}}
-            ]
-            if message.reply_to_message.caption:
-                page_content.append(
-                    {"tag": "p", "children": [message.reply_to_message.caption]}
-                )
-            page_title = f"{message.from_user.first_name} {message.from_user.last_name or ''}'s Media"
-            media_page = telegraph.create_page(title=page_title, content=page_content)
-            return await XD.edit(
-                _("grp_3").format(emo.sukses, media_page),
-                disable_web_page_preview=True,
-            )
-        except Exception as exc:
-            return await XD.edit(_("err").format(emo.gagal, exc))
-    elif message.reply_to_message.text:
-        page_title = f"{client.me.first_name} {client.me.last_name or ''}"
-        page_text = message.reply_to_message.text.replace("\n", "<br>")
-        try:
-            response = telegraph.create_page_md(title=page_title, content=page_text)
-        except Exception as exc:
-            return await XD.edit(_("err").format(emo.gagal, exc))
-        return await XD.edit(
-            _("grp_3").format(emo.sukses, response),
-            disable_web_page_preview=True,
-        )
-"""
+async def post_to_telegraph(is_media: bool, title=None, content=None, media=None):
+    telegraph = Telegraph()
+    if telegraph.get_access_token() is None:
+        await telegraph.create_account(short_name=bot.me.username)
+    if is_media:
+        # Create a Telegram Post Foto/Video
+        response = await telegraph.upload_file(media)
+        return f"https://img.yasirweb.eu.org{response[0]['src']}"
+    # Create a Telegram Post using HTML Content
+    response = await telegraph.create_page(
+        title,
+        html_content=content,
+        author_url=f"https://t.me/{bot.me.username}",
+        author_name=bot.me.username,
+    )
+    return f"https://graph.org/{response['path']}"
 
 
 async def upload_media(m):
@@ -147,6 +51,32 @@ async def upload_media(m):
     else:
         return f"{response.text}"
 
+@ky.ubot("tg")
+async def _(client: nlx, message, _):
+    emo = Emojik(client)
+    emo.initialize()
+    XD = await message.reply(_("proses").format(emo.proses))
+    if not message.reply_to_message:
+        return await XD.edit(_("grp_1").format(emo.gagal))
+    if message.reply_to_message.text:
+        page_title = f"{client.me.first_name} {client.me.last_name or ''}"
+        page_text = message.reply_to_message.text
+        page_text = page_text.replace("\n", "<br>")
+        try:
+            url = await post_to_telegraph(False, page_title, page_text)
+        except Exception as exc:
+            return await XD.edit(_("err").format(emo.gagal, exc))
+        return await XD.edit(
+            _("grp_3").format(emo.sukses, url),
+            disable_web_page_preview=True,
+        )
+    else:
+        try:
+            url = await upload_media(message)
+        except Exception as exc:
+            return await XD.edit(_("err").format(emo.gagal, exc))
+        return await XD.edit(_("grp_3").format(emo.sukses, url), disable_web_page_preview=True)
+
 
 @ky.ubot("upload|upl")
 async def _(client: nlx, message, _):
@@ -158,8 +88,6 @@ async def _(client: nlx, message, _):
         return await XD.edit(_("grp_1").format(emo.gagal))
     try:
         url = await upload_media(message)
-        return await XD.edit(
-            f"{emo.sukses} <b>File berhasil diunggah: <a href='{url}'>Klik Disini</a></b>"
-        )
     except Exception as exc:
         return await XD.edit(_("err").format(emo.gagal, exc))
+    return await XD.edit(_("grp_3").format(emo.sukses, url), disable_web_page_preview=True)
