2574d225b4c6b413d85afffd72e6694e36e382c6
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-12 16:12:28 +0700

    update plugins/global.py

--- plugins/global.py
+++ plugins/global.py
@@ -334,10 +334,11 @@ async def _(c: nlx, m, _):
 @ky.nocmd("ANTIUSER", nlx)
 @capture_err
 async def _(c, m, _):
-    # lisu = dB.get_list_from_var(c.me.id, "ANTI_USER", "USERS")
-    # user = m.from_user if m.from_user else m.sender_chat
+    lisu = dB.get_list_from_var(c.me.id, "ANTI_USER", "USERS")
+    user = m.from_user if m.from_user else m.sender_chat
     if m:
-        try:
-            return await m.delete()
-        except Exception as e:
-            return await m.reply(f"Error Gagu {str(e)}")
+        if user.id in lisu:
+            try:
+                return await m.delete()
+            except Exception as e:
+                return await m.reply(f"Error Gagu {str(e)}")
