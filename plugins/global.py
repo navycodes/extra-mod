0ceb5fc42cfcdf2ea8552dd5851a1ab0a9b817f1
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-09-12 16:45:13 +0700

    update plugins/global.py

--- plugins/global.py
+++ plugins/global.py
@@ -335,10 +335,8 @@ async def _(c: nlx, m, _):
 @capture_err
 async def _(c, m, _):
     lisu = dB.get_list_from_var(c.me.id, "ANTI_USER", "USERS")
-    user = m.from_user if m.from_user else m.sender_chat
-    if m:
-        if user.id in lisu:
-            try:
-                return await c.delete_messages(m.chat.id, user.id)
-            except Exception as e:
-                return await m.reply(f"Error Gagu {str(e)}")
+    if m.from_user.id in lisu:
+        try:
+            return await c.delete_messages(m.chat.id, user.id)
+        except Exception as e:
+            return await m.reply(f"Error Gagu {str(e)}")
